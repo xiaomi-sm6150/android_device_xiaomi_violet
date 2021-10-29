9e2e9052ac5b086538964e6328c58a4af44022c3
raghavt20 <raghavtandon2000@gmail.com>
2021-10-30 01:20:25 +0530

    violet: Basic bringup for AOSPA

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aospa_violet.mk
+
+COMMON_LUNCH_CHOICES := \
+    aospa_violet-userdebug \
+    aospa_violet-eng
